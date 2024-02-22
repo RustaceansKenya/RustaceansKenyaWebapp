5df4d7b80544b608cbf2f3c09ad2bf95d37e0120
448 OG <superuser@448.africa>
2024-02-22 09:39:39 +0300

    feat(backend): Improve file serving by using a server to serve index files

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,3 +1,16 @@
-fn main() {
-    println!("Hello, world!");
+use rocket::fs::FileServer;
+
+#[macro_use]
+extern crate rocket;
+
+#[get("/")]
+fn hello() -> &'static str {
+    "Hello, world!"
+}
+
+#[launch]
+fn rocket() -> _ {
+    rocket::build()
+        .mount("/", FileServer::from("assets/"))
+        .mount("/hello", routes![hello])
 }
